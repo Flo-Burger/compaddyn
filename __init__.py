a998a250e348e92dda0b83a75faa5f70f65001c2
Flo2306 <fburger20@gmx.de>
2025-02-26 14:49:24 +1100

    Fixing_GitHub

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# shine_lab_combined_code/__init__.py
+
+from Example_Functions import GUI
+from Example_Functions import run_all_functions
+
+# Alternatively, to control what gets imported with `from shine_lab_combined_code import *`
+__all__ = ['GUI', 'run_all_functions']
+
