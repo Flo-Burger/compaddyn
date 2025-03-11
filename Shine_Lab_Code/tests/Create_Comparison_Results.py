ece77969c27304ffd1c3ffeff5452a13ccd9c0a7
Flo2306 <fburger20@gmx.de>
2025-03-11 14:34:48 +1100

    Changing_Test_Data

--- Shine_Lab_Code/tests/Create_Comparison_Results.py
+++ Shine_Lab_Code/tests/Create_Comparison_Results.py
@@ -1,10 +1,14 @@
 import os
 import numpy as np
 from scipy.io import loadmat, savemat
-from Example_Functions import run_all_methods  
+
+import sys
+import os
+sys.path.append(os.path.abspath(".."))  # Moves one level up
+from Shine_Lab_Code.Example_Functions.run_all_functions import run_all_methods
 
 # Define paths
-TEST_DATA_PATH = "/Users/22119216/Desktop/USYD_RA_2025/Shine_Lab_Combined_Code/tests/cort_ts1c_short.mat"  
+TEST_DATA_PATH = "/Users/22119216/Desktop/USYD_RA_2025/Shine_Lab_Combined_Code/Shine_Lab_Code/Example_Data/fMRI_like_data.mat"  
 EXPECTED_RESULTS_PATH = "tests/expected_results/"  
 
 os.makedirs(EXPECTED_RESULTS_PATH, exist_ok=True)
@@ -18,5 +22,5 @@ if len(filtered_keys) != 1:
 test_data = mat_data[filtered_keys[0]]
 
 # Run the function on test data
-run_all_methods(data=test_data, output_dir=EXPECTED_RESULTS_PATH, overwrite=False)
+run_all_methods(data=test_data, output_dir=EXPECTED_RESULTS_PATH, overwrite=True)
 
