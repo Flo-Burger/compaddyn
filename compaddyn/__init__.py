06914653c58104309550f8a6fad2949aa61c4b4e
Flo2306 <fburger20@gmx.de>
2025-05-21 18:41:17 +1000

    Name_Change

---
+++ compaddyn/__init__.py
@@ -0,0 +1,8 @@
+# compaddyn/__init__.py
+
+from .Example_Functions import GUI
+from .Example_Functions import run_all_functions
+
+# Alternatively, to control what gets imported with `from compaddyn import *`
+__all__ = ['GUI', 'run_all_functions']
+
